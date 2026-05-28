b433f97d5349915a8f8e01d74209a0fac34eeb77
DAOMANHDUNG17 <manhdung17cp@gmail.com>
2026-05-28 18:02:15 +0700

    Tái cấu trúc: Thêm dịch vụ API client apiClient.ts vào thư mục modular mới

---
+++ frontend/src/services/client/complaint/apiClient.ts
@@ -0,0 +1,34 @@
+import axiosInstance from "../../../utils/axiosConfig";
+import ip from "../../../utils/ip";
+import type {
+  IComplaintsResponse,
+  IComplaintDetailResponse,
+  ICreateComplaintRequest,
+} from "./typing";
+
+const BASE_URL = `${ip}/complaints`;
+
+/**
+ * Tạo khiếu nại/phản hồi mới
+ */
+export function createComplaint(data: ICreateComplaintRequest) {
+  return axiosInstance.post(BASE_URL, data);
+}
+
+/**
+ * Lấy danh sách khiếu nại của user
+ */
+export function getComplaints(userId: number) {
+  return axiosInstance.get<IComplaintsResponse>(BASE_URL, {
+    params: { userId },
+  });
+}
+
+/**
+ * Xem chi tiết 1 khiếu nại
+ */
+export function getComplaintById(id: number, userId: number) {
+  return axiosInstance.get<IComplaintDetailResponse>(`${BASE_URL}/${id}`, {
+    params: { userId },
+  });
+}
